cb2c29a5797b23c0bfa17fd1c83f7c3f42f4290b
Nimesh9172 <nimeshdj123@gmail.com>
2023-07-29 18:08:15 +0530

    first commit

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,54 @@
+import { useEffect, useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+  const inputRef = useRef();
+
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid,setEnteredNameIsValid] = useState(false)
+  const [enteredNameIsTouched,setEnteredNameIsTouched] = useState(false)
+
+  useEffect(()=>{
+    if (enteredNameIsValid){
+      console.log(enteredNameIsValid)
+    }
+  },[enteredNameIsValid])
+
+  const inputHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameIsTouched(true)
+    if (enteredName.trim().length === 0) {
+      setEnteredNameIsValid(false)
+      return;
+    }
+    
+    console.log(enteredName, inputRef.current.value);
+    
+    setEnteredName("");
+    setEnteredNameIsValid(true)
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={`${'form-control'} ${!enteredNameIsValid && enteredNameIsTouched &&'invalid'}`}>
+        <label htmlFor="name">Your Name</label>
+        <input
+          type="text"
+          id="name"
+          onChange={inputHandler}
+          ref={inputRef}
+          value={enteredName}
+        />
+        {!enteredNameIsValid && enteredNameIsTouched && <p className="error-text">Name must not be empty</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
